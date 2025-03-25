3752cea2e6cb8e0f9fe7506207f10e303bdf7ff2
nafernandez302 <nafernandez@mi.unc.edu.ar>
2025-03-24 21:17:56 -0300

    fix: rutas cambiadas, script hace referencia a la ruta

--- run.sh
+++ run.sh
@@ -2,17 +2,17 @@
 
 # Verificar si se han pasado ambos parámetros N y name
 if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Debe proporcionar dos parámetros: N (número) y name (nombre de archivo)."
+  echo "Debe proporcionar dos parámetros: N (número) y path (ruta del archivo)."
   exit 1
 fi
 
 # Asignar valores a las variables N y name
 N=$1
-name=$2
+path=$2
 
 # Limpia los archivos y compila nuevamente
 make clean
 make N="$N"
 
 # Ejecutar el programa tiny_md y redirigir la salida al archivo output/name
-perf stat ./tiny_md > "output/${N}_${name}" 2>&1
+perf stat ./tiny_md > "output/${path}/${N}.out" 2>&1
